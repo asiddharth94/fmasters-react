a97ad79d4c1d6d2582ca77865e9a4022e5a77200
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2020-09-13 01:47:17 +0530

    added JSX

--- src/App.js
+++ src/App.js
@@ -5,29 +5,37 @@ import Pet from "./Pet";
 // Our App Component; components are basically functions which return some markup
 // Also called as Composite Component - a fancy word for the components that we create
 const App = () => {
-  return React.createElement(
-    "div", // first param - what tag is it
-    {}, // second param - attributes on the tag, e.g. {id: "container"}
-    [
-      //third-param - children of the 'div', if we want multiple chidren, we can pass all in an Array
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Sniper",
-        animal: "Dog",
-        breed: "Retriever",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Billa",
-        animal: "Cat",
-        breed: "Mixed",
-      }),
-    ]
+  //   return React.createElement(
+  //     "div", // first param - what tag is it
+  //     {}, // second param - attributes on the tag, e.g. {id: "container"}
+  //     [
+  //       //third-param - children of the 'div', if we want multiple chidren, we can pass all in an Array
+  //       React.createElement("h1", {}, "Adopt Me!"),
+  //       React.createElement(Pet, {
+  //         name: "Sniper",
+  //         animal: "Dog",
+  //         breed: "Retriever",
+  //       }),
+  //       React.createElement(Pet, {
+  //         name: "Pepper",
+  //         animal: "Bird",
+  //         breed: "Cockatiel",
+  //       }),
+  //       React.createElement(Pet, {
+  //         name: "Billa",
+  //         animal: "Cat",
+  //         breed: "Mixed",
+  //       }),
+  //     ]
+  //   );
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Sniper" animal="Dog" breed="Retriever" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Billa" animal="Cat" breed="Mixed" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
