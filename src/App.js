79c6f4da73bc1c4990d086329bc237a56209581d
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2020-09-12 23:52:11 +0530

    added tools

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-const Pet = ({name, animal, breed}) => { //using destructuring
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, name),
-            React.createElement("h2", {}, animal),
-            React.createElement("h2", {}, breed)
-        ]
-    );
-}
+const Pet = ({ name, animal, breed }) => {
+  //using destructuring
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 // Our App Component; components are basically functions which return some markup
 // Also called as Composite Component - a fancy word for the components that we create
 const App = () => {
-    return React.createElement(
-        "div", // first param - what tag is it
-        {}, // second param - attributes on the tag, e.g. {id: "container"}
-        [ //third-param - children of the 'div', if we want multiple chidren, we can pass all in an Array
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Sniper",
-                animal: "Dog",
-                breed: "Retriever"
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel"
-            }),
-            React.createElement(Pet, {
-                name: "Billa",
-                animal: "Cat",
-                breed: "Mixed"
-            })
-        ]
-    );
-}
+  return React.createElement(
+    "div", // first param - what tag is it
+    {}, // second param - attributes on the tag, e.g. {id: "container"}
+    [
+      //third-param - children of the 'div', if we want multiple chidren, we can pass all in an Array
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Sniper",
+        animal: "Dog",
+        breed: "Retriever",
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel",
+      }),
+      React.createElement(Pet, {
+        name: "Billa",
+        animal: "Cat",
+        breed: "Mixed",
+      }),
+    ]
+  );
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
